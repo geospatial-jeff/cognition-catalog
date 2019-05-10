f9bb1d98e4f3a44e6e73ae5b43bae3a13a27d683
Jeff Albrecht <geospatialjeff@gmail.com>
2019-05-10 12:09:48 -0700

    adding sat-api deployment

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
+################################
+# Setting up #
+################################
+# Clone libraries
+git clone https://github.com/geospatial-jeff/cognition-datasources.git
+git clone https://github.com/sat-utils/sat-api-deployment.git
+
+# Update deployment configurations
+python config_loader.py
 
 ################################
 # Deploy cognition-datasources #
 ################################
 
-
 # Install library
 echo "Installing Cognition-Datasources."
-git clone https://github.com/geospatial-jeff/cognition-datasources.git
 (cd cognition-datasources && python setup.py develop)
 
 # Load drivers from `config.yml`
@@ -17,13 +24,20 @@ while read -r line; do
     echo "$line"
     load_drivers+=" -d "$line""
 done <<< "$datasources"
-echo "$load_drivers"
+
 eval $load_drivers
 
 # Build deployment package
 echo "Building deployment package."
-docker build . -t cognition-datasources:latest
-docker run --rm -v $PWD:/home/cognition-datasources -it cognition-datasources:latest package-service.sh
+(cd cognition-datasources && \
+    docker build . -t cognition-datasources:latest && \
+    docker run --rm -v $PWD:/home/cognition-datasources -it cognition-datasources:latest package-service.sh && \
+    echo "Deploying to AWS." && \
+    sls deploy -v)
 
-echo "Deploying to AWS."
-sls deploy -v
+##################
+# Deploy sat-api #
+##################
+(cd sat-api-deployment && ./node_modules/.bin/kes cf deploy --region us-east-1 --template .kes/template --showOutputs)
+
+
