995e1c8041ccc34aa80a81c9ffee7e4b52399400
Jeff Albrecht <geospatialjeff@gmail.com>
2019-05-10 12:35:42 -0700

    updating config and loader

--- config_loader.py
+++ config_loader.py
@@ -14,10 +14,11 @@ class ConfigLoader(object):
             yaml.safe_dump(data, outfile, default_flow_style=False)
 
     def __init__(self):
-        self.catalog_config = os.path.join(os.path.dirname(__file__), 'config.yml')
+        self.user_config = os.path.join(os.path.dirname(__file__), 'config.yml')
         self.api_config = os.path.join(os.path.dirname(__file__), 'sat-api-deployment', '.kes', 'config.yml')
         self.cd_config = os.path.join(os.path.dirname(__file__), 'cognition-datasources', 'serverless.yml')
-        self.configs = self.load_configs(['catalog_config', 'api_config', 'cd_config'])
+        self.catalog_config = os.path.join(os.path.dirname(__file__), 'catalog', 'serverless.yml')
+        self.configs = self.load_configs(['user_config', 'api_config', 'cd_config', 'catalog_config'])
 
     def load_configs(self, file_list):
         configs = {x:self.yaml_safe_load(getattr(self, x)) for x in file_list}
@@ -26,14 +27,18 @@ class ConfigLoader(object):
     def build_configs(self):
         """Build cognition-datasources and sat-api configuration with values from catalog configuration"""
 
+        # Update cognition-catalog configuration
+        self.configs['catalog_config']['resources']['Resources']['SatAPIDeploymentBucket']['BucketName'] = self.configs['user_config']['sat-api']['bucket']
+        self.configs['catalog_config']['service'] = self.configs['user_config']['name']
+
         # Update sat-api configuration
-        self.configs['api_config']['default']['system_bucket'] = self.configs['catalog_config']['sat-api']['bucket']
-        self.configs['api_config']['default']['lambdas']['ingest']['envs']['SUBNETS'] = ' '.join(self.configs['catalog_config']['sat-api']['subnets'])
-        self.configs['api_config']['default']['lambdas']['ingest']['envs']['SECURITY_GROUPS'] = self.configs['catalog_config']['sat-api']['security-group']
+        self.configs['api_config']['default']['system_bucket'] = self.configs['user_config']['sat-api']['bucket']
+        self.configs['api_config']['default']['lambdas']['ingest']['envs']['SUBNETS'] = ' '.join(self.configs['user_config']['sat-api']['subnets'])
+        self.configs['api_config']['default']['lambdas']['ingest']['envs']['SECURITY_GROUPS'] = self.configs['user_config']['sat-api']['security-group']
 
         # Update cognition-datsources configuration
-        self.configs['cd_config']['custom']['service-name'] = self.configs['catalog_config']['cognition-datasources']['name']
-        self.configs['cd_config']['custom']['stage'] = self.configs['catalog_config']['cognition-datasources']['stage']
+        self.configs['cd_config']['custom']['service-name'] = self.configs['user_config']['cognition-datasources']['name']
+        self.configs['cd_config']['custom']['stage'] = self.configs['user_config']['cognition-datasources']['stage']
 
     def write_configs(self):
         """Write configuration files"""
